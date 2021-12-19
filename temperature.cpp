ab0fb8508bb83b4e5f4590555c8d4e6924a15761
Brian Williams <49660533+bwilliams512@users.noreply.github.com>
2021-12-18 18:08:01 -0600

    Add files via upload

---
+++ temperature.cpp
@@ -0,0 +1,20 @@
+//This project converts a temperature from Fahrenheit (F) to Celsius (C).
+
+#include <iostream>
+
+int main() {
+  
+  double tempf;
+  double tempc;
+  
+  // Ask the user
+  
+  std::cout << "Enter the temperature in Fahrenheit: ";
+
+  std::cin >> tempf;
+
+  tempc = (tempf - 32) / 1.8;
+
+  std::cout << "The temp is " << tempc << " degrees Celsius.\n";
+  
+}
